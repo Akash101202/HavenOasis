a4830cf83f0158a959784d6f801652ad96d5bc38
P Akash <p.akash101202@gmail.com>
2024-09-13 13:55:37 +0530

    Setting up Supabase

---
+++ .vite/deps/browser-VLKRZ7RP.js
@@ -0,0 +1,18 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch,
+  init_browser
+} from "./chunk-C5A7FNJB.js";
+import "./chunk-V4OQ3NZ2.js";
+init_browser();
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
+//# sourceMappingURL=browser-VLKRZ7RP.js.map

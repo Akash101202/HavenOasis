5b2133c3ff7a7fccf55b6eb075a78e23b598021c
P Akash <p.akash101202@gmail.com>
2024-10-06 00:07:54 +0530

    Authentification

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
-import { HiSquare2Stack } from "react-icons/hi2";
+import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import useDeleteCabin from "./useDeleteCabin";
 import useCreateCabin from "./useCreateCabin";
 import Modal from "../../ui/Modal";
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
 function Header() {
   const StyledHeader = styled.header`
     background-color: var(--color-grey-0);
     padding: 1.2rem 4.8rem;
     border-bottom: 1px solid var(--color-grey-100);
   `;
-  return <StyledHeader>Header</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Logout />
+    </StyledHeader>
+  );
 }
 
 export default Header;

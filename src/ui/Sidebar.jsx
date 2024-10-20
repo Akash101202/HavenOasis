bfa7c15655c824bbcd59aa80cf1e6540d4b89665
P Akash <p.akash101202@gmail.com>
2024-10-20 18:08:36 +0530

    add Data

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 function Sidebar() {
   const StyledSidebar = styled.aside`
@@ -16,6 +17,7 @@ function Sidebar() {
     <StyledSidebar>
       <Logo />
       <MainNav />
+      <Uploader />
     </StyledSidebar>
   );
 }
